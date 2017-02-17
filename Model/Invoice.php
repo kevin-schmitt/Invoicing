76254e42682a3a6c52e31664a375ccaea39e0a42
kevin-schmitt <kevin.schmitt.upjv@gmail.com>
2017-02-17 18:00:38 +0100

    MVC skeleton

---
+++ Model/Invoice.php
@@ -0,0 +1,38 @@
+<?php
+
+	class Facture{
+		
+		private $_date;
+		private $_person;
+		private $_total;
+		
+		public function __construct(){
+			
+		}
+		
+		public function getDate(){
+			return $this->_date;
+		}
+		
+		public function setDate($newDate){			
+			$this->_date = $newDate;
+		}
+		
+		public function getPerson(){
+			return $this->_person;
+		}
+		
+		public function setPerson(Person $person){			
+			$this->_person = $person;
+		}
+		
+		public function getTotal(){
+			return $this->_total;
+		}
+		
+		public function setTotal($total){			
+			$this->_total = $total;
+		}
+		
+	}
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.css">
+	</head>
+	<body lang="en">
+		
+	</body>
+</html>
