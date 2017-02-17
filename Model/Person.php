d6d942a8e524af86f21bfc1dd73dcc71a2798364
kevin-schmitt <kevin.schmitt.upjv@gmail.com>
2017-02-17 19:43:50 +0100

    update model

---
+++ Model/Person.php
@@ -0,0 +1,39 @@
+<?php
+
+	class Person{
+		
+		private $_id;
+		private $_name;
+		private $_adress;
+		
+		public function __construct(){
+			
+		}
+		
+		public function getId(){
+			return $this->_id;
+		}
+		
+		public function setId($id){			
+			$this->_id = $id;
+		}
+		
+		public function getName(){
+			return $this->_name;
+		}
+		
+		public function setName($name){			
+			$this->_name = $name;
+		}
+		
+		public function getAdress(){
+			return $this->_adress;
+		}
+		
+		public function setAdress($adress){			
+			$this->_adress = $adress;
+		}
+		
+		
+	}
+
